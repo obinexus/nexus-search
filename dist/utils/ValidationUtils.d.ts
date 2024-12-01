c8d4e4269be252e5641b6fecc5e612389162a5a2
Nnamdi Michael Okpala <obinexus@tuta.com>
2024-12-01 17:29:14 +0000

    it

--- dist/utils/ValidationUtils.d.ts
+++ dist/utils/ValidationUtils.d.ts
@@ -1,4 +1,4 @@
-import { SearchOptions, IndexConfig } from '@types';
+import { SearchOptions, IndexConfig } from '../types/index';
 export declare function validateSearchOptions(options: SearchOptions): void;
 export declare function validateIndexConfig(config: IndexConfig): void;
 export declare function validateDocument(document: any, fields: string[]): boolean;
