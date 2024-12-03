2d881fe198cfdb32b586ccf9348ba89e2c2fb30f
Nnamdi Michael Okpala <obinexus@tuta.com>
2024-12-03 11:00:05 +0000

    init

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,9 +1,7 @@
-import { SearchContext } from './internal';
 
 export * from './types';
 export * from './errors';
 export * from './events';
-export * from './internal';
 export * from './guards';
 export * from './utils';
 export * from './defaults';
@@ -11,17 +9,7 @@ export * from './document';
 
 // Export a namespace for expornal use
 export namespace NexusSearch {
-  export interface InternalConfig extends IndexConfig {
-    _id: string;
-    _created: number;
-    _updated: number;
-  }
-
-  export interface QueryContext extends SearchContext {
-    _processed: boolean;
-    _cached: boolean;
-  }
-}
+ 
 export interface SearchOptions {
     fuzzy?: boolean;
     maxResults?: number;
@@ -39,22 +27,7 @@ export interface SearchOptions {
     matches: string[];
     highlights?: Record<string, string[]>;
   }
-  
-  export interface IndexConfig {
-    name: string;
-    version: number;
-    fields: string[];
-    options?: IndexOptions;
-  }
-  
-  export interface IndexOptions {
-    caseSensitive?: boolean;
-    stemming?: boolean;
-    stopWords?: string[];
-    minWordLength?: number;
-    maxWordLength?: number;
-    fuzzyThreshold?: number;
-  }
+
   
   export interface SearchStats {
     totalResults: number;
@@ -63,4 +36,4 @@ export interface SearchOptions {
     queryComplexity: number;
   }
 
- 
+}
--- src/types/internal.ts
+++ src/types/internal.ts
@@ -1,27 +1,5 @@
 import { SearchOptions, SearchResult, SearchStats } from './models';
 
-export interface IndexNode {
-  id: string;
-  value: any;
-  score: number;
-  children: Map<string, IndexNode>;
-}
-
-export interface SearchContext {
-  query: string;
-  options: SearchOptions;
-  startTime: number;
-  results: SearchResult<any>[];
-  stats: SearchStats;
-}
-
-export interface TokenInfo {
-  value: string;
-  type: 'word' | 'operator' | 'modifier' | 'delimiter';
-  position: number;
-  length: number;
-}
-
   
 export interface IndexNode {
   id: string;
@@ -46,3 +24,19 @@ export interface TokenInfo {
 }
 
 
+  
+export interface IndexConfig {
+  name: string;
+  version: number;
+  fields: string[];
+  options?: IndexOptions;
+}
+
+export interface IndexOptions {
+  caseSensitive?: boolean;
+  stemming?: boolean;
+  stopWords?: string[];
+  minWordLength?: number;
+  maxWordLength?: number;
+  fuzzyThreshold?: number;
+}
