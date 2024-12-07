6c502eebb8619cddbc3940d684e8864b06964b28
Nnamdi Michael Okpala <okpalan@protonmail.com>
2024-12-07 01:12:49 +0000

    it

--- src/core/SearchEngine.ts
+++ src/core/SearchEngine.ts
@@ -1,9 +1,3 @@
-import { IndexManager } from './IndexManager';
-import { QueryProcessor } from './QueryProcessor';
-import { SearchStorage } from '../storage/IndexedDBService';
-import { CacheManager } from '../storage/CacheManager';
-import { validateSearchOptions } from '@/utils/ValidationUtils';
-import { IndexConfig, SearchOptions, SearchResult } from '@/types/compactablity';
 
 export class SearchEngine {
   private indexManager: IndexManager;
