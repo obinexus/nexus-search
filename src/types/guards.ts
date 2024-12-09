8d62ad3e391638cd9604f5f5235bb4f77702964e
Nnamdi Michael Okpala <obinexus@tuta.com>
2024-12-09 17:31:25 +0000

    itni

--- src/types/guards.ts
+++ src/types/guards.ts
@@ -2,28 +2,34 @@ import { IndexConfig } from "./compactability";
 import { SearchOptions, SearchResult } from "./search";
 
   
-  export function isSearchOptions(obj: unknown): obj is SearchOptions {
-    return obj && (
-      typeof obj.fuzzy === 'undefined' || typeof obj.fuzzy === 'boolean'
-    ) && (
-      typeof obj.maxResults === 'undefined' || typeof obj.maxResults === 'number'
-    );
-  }
-  
-  export function isIndexConfig(obj: unknown): obj is IndexConfig {
-    return obj && 
+export function isSearchOptions(obj: unknown): obj is SearchOptions {
+  return (
+      typeof obj === 'object' &&
+      obj !== null &&
+      (typeof obj.fuzzy === 'undefined' || typeof obj.fuzzy === 'boolean') &&
+      (typeof obj.maxResults === 'undefined' || typeof obj.maxResults === 'number')
+  );
+}
+
+export function isIndexConfig(obj: unknown): obj is IndexConfig {
+  return (
+      typeof obj === 'object' &&
+      obj !== null &&
       typeof obj.name === 'string' &&
       typeof obj.version === 'number' &&
-      Array.isArray(obj.fields);
-  }
-  
-  export function isSearchResult<T>(obj: unknown): obj is SearchResult<T> {
-    return obj &&
+      Array.isArray(obj.fields)
+  );
+}
+
+export function isSearchResult<T>(obj: unknown): obj is SearchResult<T> {
+  return (
+      typeof obj === 'object' &&
+      obj !== null &&
       'item' in obj &&
-      typeof obj.score === 'number' &&
-      Array.isArray(obj.matches);
-  }
-  
+      typeof (obj as any).score === 'number' &&
+      Array.isArray((obj as any).matches)
+  );
+}
 
   
 
