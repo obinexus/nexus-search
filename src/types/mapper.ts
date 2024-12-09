1d8253a937b8269e8283f4e74b830d18eaea7c6c
Nnamdi Michael Okpala <obinexus@tuta.com>
2024-12-09 17:24:26 +0000

    inti

--- src/types/mapper.ts
+++ src/types/mapper.ts
@@ -1,9 +1,18 @@
+import { IndexConfig, IndexedDocument } from ".";
+
 export interface MapperState {
     trie: unknown;
     dataMap: Record<string, string[]>;
+    documents: Array<{
+        key: string;
+        value: IndexedDocument;
+    }>;
+    config: IndexConfig;
 }
 
 export interface MapperOptions {
     caseSensitive?: boolean;
     normalization?: boolean;
 }
+
+
