f81adca78e77a378f1823f3c0b34e7b4d5a73e3c
Nnamdi Michael Okpala <okpalan@protonmail.com>
2024-12-21 01:15:11 +0000

    init

--- src/types/search.ts
+++ src/types/search.ts
@@ -16,7 +16,7 @@ export interface SearchOptions {
     sortOrder?: 'asc' | 'desc';
     page?: number;
     pageSize?: number;
-    regex?: RegExp;
+    regex?: boolean;
     highlight?: boolean;
     includeMatches?: boolean;
     includeScore?: boolean;
